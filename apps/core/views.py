8e743af71ea46e0147eaa7c0fbebd806c9f9371a
Deyby Camacho <camachodeybygerman@gmail.com>
2025-12-07 13:15:23 -0300

    feat: Implemented about me. - page abou_me.html complete with bootstrap and css. - views + urls to redirection on click in the footer.

--- apps/core/views.py
+++ apps/core/views.py
@@ -5,6 +5,7 @@ Core views - Shared functionality
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
 
 @csrf_exempt
 @require_GET
@@ -25,3 +26,10 @@ def health_check(request):
         ],
         'environment': 'docker'
     })
+
+
+class AboutMeView(TemplateView):
+    """
+    Vista para la página Acerca de Mí
+    """
+    template_name = 'about_me.html'
